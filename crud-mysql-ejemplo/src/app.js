c4ebb8e97a23a497682379b1c85ab5d723e86f71
JoseRamonGraciaCalderon <48308714+JoseRamonGraciaCalderon@users.noreply.github.com>
2019-11-09 18:20:11 -0800

    Configurando servidor Nodejs

---
+++ crud-mysql-ejemplo/src/app.js
@@ -0,0 +1,27 @@
+const path= require('path');
+const express =require('express');
+const mysql= require('mysql');
+const myConnection = require('express-myconnection');
+const app= express();
+ 
+
+//importar rutas
+const indiceRutas= require('./rutas/index.js');
+
+//configuraciones
+
+app.set('port',process.env.PORT || 3000);
+app.set('view engine','ejs');
+//obtiene la direccion en la carpeta con path y a path le concateno la carpeta
+app.set('views',path.join(__dirname,'vistas' ));
+
+//middleware
+//app.use();
+
+//usamos las rutas
+app.use('/',indiceRutas);
+
+//escucha el puerto y manda el mensaje 
+app.listen(app.get('port'),() =>{
+	console.log('Escuchando en el puerto 3000');
+});
